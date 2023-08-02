26975abfd0f448d3df2d0f1bb988828364fa0470
nauval01~ <nauvalmulyana242@gmail.com>
2023-08-02 20:44:13 +0700

    test

---
+++ src/userComponents/navbar/MainNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainNavbar = () => {
+    return (
+        <div>
+            <p>this navbar</p>
+        </div>
+    )
+}
+
+export default MainNavbar;
---
+++ src/userPages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainNavbar from '../userComponents/navbar/MainNavbar'
+
+const Home = () => {
+    return (
+        <div>
+            <MainNavbar />
+        </div>
+    )
+}
+
+export default Home
