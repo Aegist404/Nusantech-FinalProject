9d8f904d3c64ab71fbff0c9b583a2a8fafb45788
muhamad afif mustafa <muhamadafifmustafa83@gmail.com>
2023-08-20 15:05:22 +0700

    Komentar Kuis

--- src/userPages/Home.jsx
+++ src/userPages/Home.jsx
@@ -1,15 +1,18 @@
+
+import Komentar from '../userComponents/komentarKuis/komentarKuis'
 import MainNavbar from '../userComponents/navbar/MainNavbar'
 // import KategoriPelatihan from '../userComponents/navbar/kategoriPelatihan/kategoriPelatihan'
-import ProductCard from '../userComponents/navbar/Pelatihan/Pelatihan'
-import MainFooter from '../userComponents/footer/mainFooter'
+// import ProductCard from '../userComponents/navbar/Pelatihan/Pelatihan'
+// import MainFooter from '../userComponents/footer/mainFooter'
 // import BannerPelatihan from '../userComponents/bannerPelatihan/bannerPelatihan'
 const Home = () => {
     return (
         <div>
             <MainNavbar />
-            <KategoriPelatihan/>
-            <ProductCard/>
-            <MainFooter />
+            <Komentar/>
+            {/* <KategoriPelatihan/> */}
+            {/* <ProductCard/>
+            <MainFooter /> */}
             {/* <BannerPelatihan /> */}
         </div>
     )
