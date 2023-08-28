cc683a96ebaf3955ef01a861ab8a6e67a2a4895d
adhi <adhirahman99@gmail.com>
2023-08-28 12:26:42 +0700

    menambahkan rating coment

--- src/userPages/Registrasi.jsx
+++ src/userPages/Registrasi.jsx
@@ -1,12 +1,14 @@
 import Registrasi from "../userComponents/Registrasi/RegistrasiPembeli";
 import MainFooter from "../userComponents/navigation/MainFooter";
 import MainNavbar from "../userComponents/navigation/MainNavbar";
+import CommentSection from "../userComponents/komentarKuis/ComentRating";
 const RegistrasiProfile = ( ) => {
     return(
         <div>
-            <MainNavbar/>
+            {/* <MainNavbar/>
             <Registrasi/>
-            <MainFooter/>
+            <MainFooter/> */}
+            <CommentSection/>
         </div>
     )
 }
